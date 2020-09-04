1574d2de3d6b3e0abc3280d5401918ccd1c4ee6d
Vince Williams <williamsvm20@gmail.com>
2020-09-04 00:29:52 -0400

    day 1 work done
    initial repl complete
    player.py is created
    room.py is created

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,10 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mouth beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -39,6 +39,10 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+    
+player = Player('Bob', room['outside'])
+
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +53,35 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+print(player.about())
+
+direction = ''
+
+while direction !='q':
+
+    direction = input("Please select a direction to move: \n (N)orth, (S)outh, (E)ast, (W)est. ")
+
+    try:
+
+        direction = direction
+
+        if direction == 'n':
+            if player.room.n_to:
+                player.room = player.room.n_to
+                print(f"\n{player.about()}\n")
+        elif direction == 's':
+            if player.room.s_to:
+                player.room = player.room.s_to
+                print(f"\n{player.about()}\n")
+        elif direction == 'e':
+            if player.room.e_to:
+                player.room = player.room.e_to
+                print(f"\n{player.about()}\n")
+        elif direction == 'w':
+            if player.room.w_to:
+                player.room = player.room.w_to
+                print(f"\n{player.about()}\n")
+    except:
+        print('\nThe way is blocked!\n')
+
--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+from room import Room 
+
+class Player:
+    def __init__(self, name, starting_room):
+        self.name = name
+        self.room = starting_room
+        self.inventory = []
+
+    def __str__(self):
+        return f"{self.name} is in the {self.room.describe()}"
+
+    def about(self):
+        return self.__str__()
